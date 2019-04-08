bbd5f6c43471e6f434695c12b76cbb3362702395
karla fragoso cerecedo <1717110248@utectulancingo.edu.mx>
2019-04-08 09:08:22 -0500

    first commit

---
+++ config.py
@@ -0,0 +1,27 @@
+import web
+"""
+db_host = 'localhost'
+db_name = 'ferreteriamago'
+db_user = 'ferreteria'
+db_pw = 'ferreteria.2019'
+
+db = web.database(
+    dbn='mysql',
+    host=db_host,
+    db=db_name,
+    user=db_user,
+    pw=db_pw
+    )
+"""
+db_host = 'e764qqay0xlsc4cz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+db_name = 'gce70i0ywa34rsob'
+db_user = 'pjk83pkmz0oyc0po'
+db_pw = '	mns0vcw6l6jj9ge9'
+
+db = web.database(
+    dbn='mysql',
+    host=db_host,
+    db=db_name,
+    user=db_user,
+    pw=db_pw
+    )
