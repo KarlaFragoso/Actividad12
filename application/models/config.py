83c8ba8057329b0ee6c6b5f5639a1e34006a7c94
karla fragoso cerecedo <1717110248@utectulancingo.edu.mx>
2019-04-08 09:44:09 -0500

    modificaciones

--- application/models/config.py
+++ application/models/config.py
@@ -1,18 +1,5 @@
 import web
-"""
-db_host = 'localhost'
-db_name = 'ferreteriamago'
-db_user = 'ferreteria'
-db_pw = 'ferreteria.2019'
 
-db = web.database(
-    dbn='mysql',
-    host=db_host,
-    db=db_name,
-    user=db_user,
-    pw=db_pw
-    )
-"""
 db_host = 'bmsyhziszmhf61g1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
 db_name = 'emu896o5x5926xvn'
 db_user = 'dzlmadj8085s2x4i'
