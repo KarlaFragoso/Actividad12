9a0d603df6856ccd578d6d4bee0904692f9fea4f
karla fragoso cerecedo <1717110248@utectulancingo.edu.mx>
2019-04-08 09:14:21 -0500

    cambios

--- config.py
+++ config.py
@@ -13,10 +13,10 @@ db = web.database(
     pw=db_pw
     )
 """
-db_host = 'e764qqay0xlsc4cz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
-db_name = 'gce70i0ywa34rsob'
-db_user = 'pjk83pkmz0oyc0po'
-db_pw = '	mns0vcw6l6jj9ge9'
+db_host = 'bmsyhziszmhf61g1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+db_name = 'emu896o5x5926xvn'
+db_user = 'dzlmadj8085s2x4i'
+db_pw = 'jg1j0yzbm7s82def'
 
 db = web.database(
     dbn='mysql',
